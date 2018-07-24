2719736401ff86d690cf3bf94f69ee6c5cda82be
leoremigious <36755403+leoremigious@users.noreply.github.com>
2018-07-24 12:05:37 +0530

    Create armstrong bw 2.py

---
+++ armstrong bw 2.py
@@ -0,0 +1,16 @@
+lower = 1
+upper = 10000
+ 
+for num in range(lower,upper + 1):
+ 
+   sum = 0
+ 
+ 
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if num == sum:
+       print(num)
